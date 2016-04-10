bf2dcd4568b7e0995f19e571b801f553873b786f
ichenq <ichenq@outlook.com>
2016-04-10 10:33:43 +0800

    new dir

--- src/erlang/afile_client.erl
+++ src/erlang/afile_client.erl
@@ -1,13 +1,15 @@
 -module(afile_client).
 -export([ls/1, get_file/2]).
 
+%% list files
 ls(Server) ->
 	Server ! {self(), list_dir},
-	receive 
+	receive
 		{Server, FileList} ->
 			FileList
 	end.
 
+%% get file content
 get_file(Server, File) ->
 	Server ! {self(), {get_file, File}},
 	receive
