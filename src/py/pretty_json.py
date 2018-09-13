89f91965e2e869f390729d2910104c303d54130a
JC <ichenq@outlook.com>
2018-09-13 13:06:11 +0800

    Update pretty_json.py

--- src/py/pretty_json.py
+++ src/py/pretty_json.py
@@ -4,25 +4,35 @@
 import json
 import codecs
 import argparse
+import os
 
+
+def pretty_file(filename):
+    f = open(filename, 'r')
+    obj = json.loads(f.read())
+    f.close()
+    content = json.dumps(obj, sort_keys=False, indent=2, allow_nan=False, ensure_ascii=False)    
+    f = codecs.open(filename, 'w', 'utf-8')
+    f.write(content)
+    f.close()
+    print('Saved to ' + filename)
+        
 
 def run(args):
-    assert len(args.input) > 0
-    f = open(args.input, 'r')
-    obj = json.loads(f.read())
-    content = json.dumps(obj, sort_keys=True, indent=args.indent, allow_nan=False, ensure_ascii=False)
-    if args.output is None:
-        print content
-    else:
-        f = codecs.open(args.output, 'w', 'utf-8')
-        f.write(content)
-        f.close()
-        print('Saved to ' + args.output)
+    rootdir = args.dir
+    for root, dirs, files in os.walk(rootdir):
+        for filename in files:
+            if len(root) > 0:
+                filename = root + os.sep + filename
+            if filename.endswith('.json'):
+                print(filename)
+                pretty_file(filename)
+
 
 
 def main():
     parser = argparse.ArgumentParser(description="Pretty print JSON text")
-    parser.add_argument("-i", "--input", help="Input JSON text file")
+    parser.add_argument("-d", "--dir", help="Input JSON text file")
     parser.add_argument("-t", "--indent", help="Indent tab size", type=int, default=2)
     parser.add_argument("-o", "--output", help="Input JSON text file")
     args = parser.parse_args()
