10247a05f9dc32b0d9876ce7eed7e157a755e111
simon.chen <ichenq@outlook.com>
2020-03-08 15:05:09 +0800

    Move js code

---
+++ src/js/util.js
@@ -0,0 +1,12 @@
+
+function getNowTime() {
+    let date = new Date();
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    let hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    let minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    let second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    let milsec = date.getMilliseconds();
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second + "." + milsec;
+}
